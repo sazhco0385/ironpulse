d5d91f708bb4306f62d9591ac345ccf04905bc18
sazhco0385 <sazhco0385@gmail.com>
2026-04-02 05:28:28 +0200

    Revert metro.config.js to simple config (health.ts fixed)

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,3 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
 
-const config = getDefaultConfig(__dirname);
-
-// Mock react-native-health (iOS-only) so Android bundling doesn't fail
-config.resolver.extraNodeModules = {
-  "react-native-health": path.resolve(__dirname, "lib/health-ios-stub.js"),
-};
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
